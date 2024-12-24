c2d6f81fb05acc824b1194de5fc195c3764d5fb3
Anuj9675 <anujupadhyay6265@gmail.com>
2024-12-24 12:11:34 +0530

    update: change the size of call button

--- src/components/callbutton/indedx.tsx
+++ src/components/callbutton/indedx.tsx
@@ -4,7 +4,7 @@ const CallButton = () => {
   return (
     <a
       href="tel:+91 86307 15936" 
-      className=" bg-gradient-to-r from-orange-400 to-orange-500 text-white p-3 rounded-md shadow-lg hover:from-orange-500 hover:to-orange-600 transition-all duration-300 ease-in-out transform hover:scale-105 flex flex-col md:flex-row items-center justify-center md:justify-start md:space-x-2 w-16 h-16 md:w-auto md:h-auto"
+      className=" bg-gradient-to-r from-orange-400 to-orange-500 text-white p-3 rounded-md shadow-lg hover:from-orange-500 hover:to-orange-600 transition-all duration-300 ease-in-out transform hover:scale-105 flex flex-col md:flex-row items-center justify-center md:justify-start md:space-x-2 w-12 h-12 md:w-auto md:h-auto"
       aria-label="Buy on Phone"
     >
       <svg 
@@ -21,7 +21,7 @@ const CallButton = () => {
           d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" 
         />
       </svg>
-      <div className="flex flex-col items-center md:items-start">
+      <div className="md:block hidden flex-col items-center md:items-start">
         <p className="font-semibold text-[10px] md:text-[16px] leading-tight text-center md:text-left">
           Buy On
           <span className="hidden md:inline"> Phone</span>
